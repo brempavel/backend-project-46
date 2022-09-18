71450e83dc0faff25ee4c5d69d3977ca7c16a349
Pavlo Brem <brempavel@gmail.com>
2022-09-18 18:57:40 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,22 @@
-import { genDiff } from '../bin/gendiff';
+import { genDiff } from '../src';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getPath = (name) => path.join(__dirname, '__fixtures__', name);
 
 test('plain JSON', () => {
   const result = `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-    }`;
-    console.log(genDiff('__fixtures__ /file1.json', '__fixtures__ / file2.json'));
-  expect(genDiff('__fixtures__ /file1.json', '__fixtures__ / file2.json')).toEqual(
-    result
-  );
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`;
+  expect(
+    genDiff(getPath('file1.json'), getPath('file2.json'))
+  ).toEqual(result);
 });
