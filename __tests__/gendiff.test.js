a543c2eb3792b85420038cd566b557b097cf15eb
Pavlo Brem <brempavel@gmail.com>
2022-09-19 21:22:14 +0300

    fix expected output

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,13 +10,6 @@ const __dirname = path.dirname(__filename);
 const getPath = (name) => path.join(__dirname, '__fixtures__', name);
 
 test('plain JSON', () => {
-  const result = `{
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`;
-  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual(result);
+  const expected = fs.readFileSync(getPath('plainResult.txt'), 'utf-8');
+  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual(expected);
 });
