a59a5b04cf77fa0d0b76729ce067436893a4cf8f
Pavlo Brem <brempavel@gmail.com>
2022-09-19 21:44:45 +0300

    changed fixtures route

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,7 +7,7 @@ import genDiff from '../src/index';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getPath = (name) => path.join(__dirname, '__fixtures__', name);
+const getPath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
 test('plain JSON', () => {
   const expected = fs.readFileSync(getPath('plainResult.txt'), 'utf-8');
