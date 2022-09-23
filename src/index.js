18bb8500f0f8ec7517b73b56e7362a2e04124c1d
Pavlo Brem <brempavel@gmail.com>
2022-09-23 14:49:27 +0300

    add new import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import parse from './parsers.js';
 import getDiff from './getdiff.js';
-import stylish from './stylish.js';
+import format from './formatters/index.js'
 
-export default (filepath1, filepath2, format = 'stylish') => {
+export default (filepath1, filepath2, formatName = 'stylish') => {
   const oldData = fs.readFileSync(filepath1, 'utf-8');
   const newData = fs.readFileSync(filepath2, 'utf-8');
 
@@ -14,13 +14,7 @@ export default (filepath1, filepath2, format = 'stylish') => {
   const newParsedData = parse(newData, newExtension);
 
   const rawDifference = getDiff(oldParsedData, newParsedData);
-  let difference;
-  switch (format) {
-    case 'stylish':
-      difference = stylish(rawDifference);
-      break;
-    default:
-      throw new Error(`Unknown format type: ${format}`);
-  }
+  const difference = format(rawDifference, formatName);
+  
   return difference;
 };
