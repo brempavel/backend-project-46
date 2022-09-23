fcef3a42333cf0bd2d1126015860d6fe34330241
Pavlo Brem <brempavel@gmail.com>
2022-09-23 16:50:53 +0300

    fix extension getting method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import parse from './parsers.js';
 import getDiff from './getdiff.js';
 import format from './formatters/index.js';
@@ -7,8 +8,8 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
   const oldData = fs.readFileSync(filepath1, 'utf-8');
   const newData = fs.readFileSync(filepath2, 'utf-8');
 
-  const oldExtension = filepath1.split('.').pop();
-  const newExtension = filepath2.split('.').pop();
+  const oldExtension = path.extname(filepath1).slice(1);
+  const newExtension = path.extname(filepath2).slice(1);
 
   const oldParsedData = parse(oldData, oldExtension);
   const newParsedData = parse(newData, newExtension);
