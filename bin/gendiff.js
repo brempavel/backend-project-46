640bcbbd632079941efd918d0e79efb45d27436e
Pavlo Brem <brempavel@gmail.com>
2022-10-01 14:43:27 +0300

    fix arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,8 +8,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((path1, path2, options) => {
